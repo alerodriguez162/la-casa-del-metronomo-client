8f7295385fdf8738b1d50f48b53174dfe6ef4134
alerodriguez162 <alerodriguez162@gmail.com>
2021-10-22 15:52:07 -0500

    Scroll to top

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import OrderHistory from "./components/Profile/OrderHistory";
 import useAuth from "./Hooks/isLoggedIn";
 import AuthRoute from "./Routes/AuthRoute";
 import PrivateRoute from "./Routes/PrivatedRoute";
+import ScrollToTop from "./Utils/ScrollToTop";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC);
 
@@ -36,45 +37,47 @@ function App({ location }) {
     <>
       <Elements stripe={stripePromise}>
         {location.pathname.includes("admin") || location.pathname.includes("login") || location.pathname.includes("register") || location.pathname.includes("summary") ? null : <Header />}
+        <ScrollToTop>
+          <Switch>
+            <AuthRoute exact path="/login" component={Login} />
+            <AuthRoute exact path="/register" component={SignUp} />
 
-        <Switch>
-          <AuthRoute exact path="/login" component={Login} />
-          <AuthRoute exact path="/register" component={SignUp} />
+            <Route exact path="/" component={Home} />
 
-          <Route exact path="/" component={Home} />
+            <Route exact path="/products" component={PLP} />
+            <Route exact path="/product/:id" component={PDP} />
 
-          <Route exact path="/products" component={PLP} />
-          <Route exact path="/product/:id" component={PDP} />
+            <PrivateRoute exact path="/cart" component={Cart} />
 
-          <PrivateRoute exact path="/cart" component={Cart} />
+            <PrivateRoute exact path="/checkout" component={Checkout} />
+            <PrivateRoute exact path="/checkout/summary" component={Summary} />
 
-          <PrivateRoute exact path="/checkout" component={Checkout} />
-          <PrivateRoute exact path="/checkout/summary" component={Summary} />
+            <PrivateRoute exact path="/orders" component={OrderHistory} />
 
-          <PrivateRoute exact path="/orders" component={OrderHistory} />
+            {/* <AdminRoute path="/admin" /> */}
 
-          {/* <AdminRoute path="/admin" /> */}
+            <Route exact path="/who-whe-are" component={WhoWheAre} />
+            <Route
+              path="/admin"
+              render={(props) => {
+                if (currentUser.roles !== "admin") return <Redirect to="/" />;
+                return (
+                  <>
+                    <Admin>
+                      <Route exact path={`${props.match.url}/users`} component={Users} />
+                      <Route exact path={`${props.match.url}/categories`} component={Categories} />
+                      <Route exact path={`${props.match.url}/products`} component={Products} />
+                    </Admin>
+                  </>
+                );
+              }}
+            />
+            <Route exact path="/terms-conditions" component={TermsAndConditions} />
+            <Route exact path="/privacy" component={Privacy} />
+            <Route path="*" component={ErrorPage} />
+          </Switch>
+        </ScrollToTop>
 
-          <Route exact path="/who-whe-are" component={WhoWheAre} />
-          <Route
-            path="/admin"
-            render={(props) => {
-              if (currentUser.roles !== "admin") return <Redirect to="/" />;
-              return (
-                <>
-                  <Admin>
-                    <Route exact path={`${props.match.url}/users`} component={Users} />
-                    <Route exact path={`${props.match.url}/categories`} component={Categories} />
-                    <Route exact path={`${props.match.url}/products`} component={Products} />
-                  </Admin>
-                </>
-              );
-            }}
-          />
-          <Route exact path="/terms-conditions" component={TermsAndConditions} />
-          <Route exact path="/privacy" component={Privacy} />
-          <Route path="*" component={ErrorPage} />
-        </Switch>
         <ToastContainer position="bottom-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
         {location.pathname.includes("admin") || location.pathname.includes("login") || location.pathname.includes("register") || location.pathname.includes("summary") ? null : <Footer />}
       </Elements>
